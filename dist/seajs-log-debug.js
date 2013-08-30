beb5be4dd1d5372982dff1bd2543edfa1ad1fba7
popomore <sakura9515@gmail.com>
2013-08-30 22:36:04 +0800

    rebuild using seatools

--- dist/seajs-log-debug.js
+++ dist/seajs-log-debug.js
@@ -1,28 +1,17 @@
 /**
  * The Sea.js plugin to provide log function
  */
-(function(seajs, global) {
+define("seajs/seajs-log/1.0.0/seajs-log-debug", [], function() {
+    var data = seajs.data;
+    // The safe wrapper for `console.xxx` functions
+    // log("message") ==> console.log("message")
+    // log("message", "warn") ==> console.warn("message")
+    seajs.log = function(msg, type) {
+        window.console && (// Do NOT print `log(msg)` in non-debug mode
+        type || data.debug) && // Set the default value of type
+        console[type || (type = "log")] && // Call native method of console
+        console[type](msg);
+    };
+});
 
-  var data = seajs.data
-
-
-  // The safe wrapper for `console.xxx` functions
-  // log("message") ==> console.log("message")
-  // log("message", "warn") ==> console.warn("message")
-  seajs.log = function(msg, type) {
-
-    global.console &&
-      // Do NOT print `log(msg)` in non-debug mode
-        (type || data.debug) &&
-      // Set the default value of type
-        (console[type || (type = "log")]) &&
-      // Call native method of console
-    console[type](msg)
-  }
-
-
-  // Register as module
-  define("seajs-log-debug", [], {})
-
-})(seajs, this);
-
+seajs.use("seajs/seajs-log/1.0.0/seajs-log-debug");
