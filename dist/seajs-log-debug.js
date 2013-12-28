0ea39e4261126ab9166b21c0fc82cdbbd0506146
popomore <sakura9515@gmail.com>
2013-12-28 04:52:39 -0800

    Update documentation

--- dist/seajs-log-debug.js
+++ dist/seajs-log-debug.js
@@ -1,17 +1,23 @@
+(function(){
 /**
  * The Sea.js plugin to provide log function
  */
-define("seajs/seajs-log/1.0.0/seajs-log-debug", [], function() {
-    var data = seajs.data;
-    // The safe wrapper for `console.xxx` functions
-    // log("message") ==> console.log("message")
-    // log("message", "warn") ==> console.warn("message")
-    seajs.log = function(msg, type) {
-        window.console && (// Do NOT print `log(msg)` in non-debug mode
-        type || data.debug) && // Set the default value of type
-        console[type || (type = "log")] && // Call native method of console
-        console[type](msg);
-    };
-});
+var data = seajs.data
 
-seajs.use("seajs/seajs-log/1.0.0/seajs-log-debug");
+
+// The safe wrapper for `console.xxx` functions
+// log("message") ==> console.log("message")
+// log("message", "warn") ==> console.warn("message")
+seajs.log = function(msg, type) {
+
+  window.console &&
+    // Do NOT print `log(msg)` in non-debug mode
+      (type || data.debug) &&
+    // Set the default value of type
+      (console[type || (type = "log")]) &&
+    // Call native method of console
+  console[type](msg)
+}
+
+define("seajs/seajs-log/1.0.1/seajs-log-debug", [], {});
+})();
--- src/seajs-log.js
+++ src/seajs-log.js
@@ -1,23 +1,19 @@
 /**
  * The Sea.js plugin to provide log function
  */
-define(function() {
-
-  var data = seajs.data
+var data = seajs.data
 
 
-  // The safe wrapper for `console.xxx` functions
-  // log("message") ==> console.log("message")
-  // log("message", "warn") ==> console.warn("message")
-  seajs.log = function(msg, type) {
+// The safe wrapper for `console.xxx` functions
+// log("message") ==> console.log("message")
+// log("message", "warn") ==> console.warn("message")
+seajs.log = function(msg, type) {
 
-    window.console &&
-      // Do NOT print `log(msg)` in non-debug mode
-        (type || data.debug) &&
-      // Set the default value of type
-        (console[type || (type = "log")]) &&
-      // Call native method of console
-    console[type](msg)
-  }
-
-});
+  window.console &&
+    // Do NOT print `log(msg)` in non-debug mode
+      (type || data.debug) &&
+    // Set the default value of type
+      (console[type || (type = "log")]) &&
+    // Call native method of console
+  console[type](msg)
+}
