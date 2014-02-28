b6cb995be28338f050209e9853032721a356f04f
SamirGuo <still_wind@126.com>
2014-02-28 17:42:43 +0800

    我认为把debug和window.console放到外面效率会更高一些。
    这样就是代码显得长了一点，哈哈。但是在函数主体里面少了两个判断。

--- src/seajs-log.js
+++ src/seajs-log.js
@@ -7,13 +7,32 @@ var data = seajs.data
 // The safe wrapper for `console.xxx` functions
 // log("message") ==> console.log("message")
 // log("message", "warn") ==> console.warn("message")
-seajs.log = function(msg, type) {
+// seajs.log = function(msg, type) {
 
-  window.console &&
+//   window.console &&
+//     // Do NOT print `log(msg)` in non-debug mode
+//       (type || data.debug) &&
+//     // Set the default value of type
+//       (console[type || (type = "log")]) &&
+//     // Call native method of console
+//   console[type](msg)
+// }
+if(window.console&&data.debug){
+  function(msg, type) {
     // Do NOT print `log(msg)` in non-debug mode
-      (type || data.debug) &&
     // Set the default value of type
       (console[type || (type = "log")]) &&
     // Call native method of console
   console[type](msg)
+  }
+}else{
+  function(msg, type) {
+  window.console &&
+    // Do NOT print `log(msg)` in non-debug mode
+      type &&
+    // Set the default value of type
+      (console[type = "log"]) &&
+    // Call native method of console
+  console[type](msg)
+ }
 }
